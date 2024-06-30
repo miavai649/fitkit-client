0e133c12133fcb2570eb0f8e8d57a0d65720459c
Tanmoy-Parvez <tanmoyparvez23@gmail.com>
2024-06-30 15:36:53 +0600

    feat: simple counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
+import { useState } from "react";
 import { Button } from "./components/ui/button";
 
 const App = () => {
+  const [count, setCount] = useState(0);
   return (
-    <div>
-      <h1 className="text-4xl text-center text-red-500">Home Page</h1>
-      <Button>Click</Button>
+    <div className="flex h-screen w-full justify-center items-center ">
+      <div className="flex items-center border border-sky-500 rounded-md p-12">
+        <Button className="bg-green-500" onClick={() => setCount(count + 1)}>
+          Increment
+        </Button>
+        <h1 className="mx-10">{count}</h1>
+        <Button className="bg-red-500" onClick={() => setCount(count - 1)}>
+          Decrement
+        </Button>
+      </div>
     </div>
   );
 };
