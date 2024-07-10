ceca68f682838daa75ce8d52b0676acbe6c20aaa
miavai649 <mahmudulhaquenoor@gmail.com>
2024-07-11 01:36:58 +0600

    category card modified

--- src/components/Categories/Categories.tsx
+++ src/components/Categories/Categories.tsx
@@ -14,7 +14,9 @@ const Categories = () => {
       <h3 className='text-2xl md:text-3xl lg:text-4xl font-bold mb-8'>
         Categories for You
       </h3>
-      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4'>
+      <div
+        className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4'
+        >
         {categories.map((category) => (
           <CategoryCard
             key={category.name}
--- src/components/Categories/CategoryCard.tsx
+++ src/components/Categories/CategoryCard.tsx
@@ -2,15 +2,17 @@ import { TCategoryCardProps } from '@/types'
 
 const CategoryCard = ({ image, name }: TCategoryCardProps) => {
   return (
-    <div className='relative w-full h-[280px] overflow-hidden'>
+    <div className='relative w-full h-[280px] overflow-hidden rounded-lg group cursor-pointer'>
       <img
         src={image}
         alt='Category Image'
-        className='w-full h-full object-cover'
+        className='w-full h-full object-cover transition-transform duration-500 group-hover:scale-110'
       />
-      <div className='absolute inset-0 bg-black bg-opacity-75'></div>
+      <div className='absolute inset-0 bg-black bg-opacity-50 transition-opacity duration-500 group-hover:bg-opacity-75'></div>
       <div className='absolute bottom-0 left-0 p-4'>
-        <h3 className='text-white text-3xl font-bold'>{name}</h3>
+        <h3 className='text-white text-3xl font-bold transition-transform duration-500 group-hover:translate-y-1'>
+          {name}
+        </h3>
       </div>
     </div>
   )
