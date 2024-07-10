6c29b15ce1fd3ac1a9ab257f322f7f9267cf2349
miavai649 <mahmudulhaquenoor@gmail.com>
2024-07-11 00:10:46 +0600

    file structure modified

---
+++ src/components/Categories/Categories.tsx
@@ -0,0 +1,11 @@
+const Categories = () => {
+  return (
+    <div className='container'>
+      <h3 className='text-xl md:text-2xl lg:text-3xl font-bold my-4'>
+        Categories for You
+      </h3>
+    </div>
+  )
+}
+
+export default Categories
---
+++ src/components/Categories/CategoryCard.tsx
@@ -0,0 +1,9 @@
+const CategoryCard = () => {
+  return (
+    <div>
+      <h1>This is CategoryCard component</h1>
+    </div>
+  )
+}
+
+export default CategoryCard
