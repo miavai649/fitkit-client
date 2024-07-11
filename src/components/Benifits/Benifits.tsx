2e80c0147b9c574bf0e9df56c54207aabae583c1
miavai649 <mahmudulhaquenoor@gmail.com>
2024-07-11 17:08:24 +0600

    aos animation added in category card

--- src/components/Benifits/Benifits.tsx
+++ src/components/Benifits/Benifits.tsx
@@ -1,4 +1,4 @@
-import { cardioImg, gearImg, highQuality, weightsImg } from '@/assets'
+import { achiveGoal, happyWorkout, healthyLife, highQuality } from '@/assets'
 import {
   CheckCircleIcon,
   HeartIcon,
@@ -8,45 +8,45 @@ import {
 
 const benefits = [
   {
-    icon: <CheckCircleIcon className='h-12 w-12 text-primary' />,
+    icon: <CheckCircleIcon className='h-8 w-8 md:h-10 md:w-10 text-primary' />,
     title: 'High Quality Equipment',
     description:
       'Our products are made from durable materials to ensure long-lasting use.',
     img: highQuality
   },
   {
-    icon: <HeartIcon className='h-12 w-12 text-primary' />,
+    icon: <HeartIcon className='h-8 w-8 md:h-10 md:w-10 text-primary' />,
     title: 'Boosts Health',
     description: 'Regular use improves your overall health and well-being.',
-    img: cardioImg
+    img: healthyLife
   },
   {
-    icon: <TrophyIcon className='h-12 w-12 text-primary' />,
+    icon: <TrophyIcon className='h-8 w-8 md:h-10 md:w-10 text-primary' />,
     title: 'Achieve Your Goals',
     description:
       'Designed to help you reach your fitness milestones efficiently.',
-    img: gearImg
+    img: achiveGoal
   },
   {
-    icon: <SparklesIcon className='h-12 w-12 text-primary' />,
+    icon: <SparklesIcon className='h-8 w-8 md:h-10 md:w-10 text-primary' />,
     title: 'Enjoy Your Workouts',
     description: 'Make your fitness journey enjoyable and satisfying.',
-    img: weightsImg
+    img: happyWorkout
   }
 ]
 
 const Benefits = () => {
   return (
-    <section className='py-16 bg-primary-100 mb-7 md:mb-14'>
+    <section className='py-8 md:py-16 bg-primary-100 mb-5 md:mb-14'>
       <div className='container mx-auto text-center'>
-        <h2 className='text-2xl md:text-3xl font-bold lg:text-4xl mb-2 md:mb-4'>
+        <h2 className='text-2xl md:text-3xl lg:text-4xl font-bold  mb-2 md:mb-4'>
           Unleash Your Potential
         </h2>
-        <p className='text-lg md:text-xl text-gray-700 mb-4 mb:mb-8'>
+        <p className='text-base md:text-xl text-primary-400 mb-4 md:mb-8'>
           Achieve your fitness goals with our state-of-the-art equipment and
           accessories.
         </p>
-        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8'>
+        <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 md:gap-8'>
           {benefits.map((benefit, index) => (
             <div
               key={index}
@@ -54,12 +54,16 @@ const Benefits = () => {
               <img
                 src={benefit.img}
                 alt={benefit.title}
-                className='w-full h-48 object-cover'
+                className='w-full h-40 sm:h-48 object-cover'
               />
-              <div className='p-6'>
-                <div className='text-primary mb-4'>{benefit.icon}</div>
-                <h3 className='text-2xl font-semibold mb-2'>{benefit.title}</h3>
-                <p className='text-gray-700'>{benefit.description}</p>
+              <div className='p-4 md:p-6'>
+                <div className='text-primary mb-2 md:mb-4'>{benefit.icon}</div>
+                <h3 className='text-xl md:text-2xl font-semibold mb-2'>
+                  {benefit.title}
+                </h3>
+                <p className='text-gray-700 text-sm md:text-base'>
+                  {benefit.description}
+                </p>
               </div>
             </div>
           ))}
--- src/components/Categories/CategoryCard.tsx
+++ src/components/Categories/CategoryCard.tsx
@@ -1,18 +1,30 @@
 import { TCategoryCardProps } from '@/types'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
+import { useEffect } from 'react'
 
 const CategoryCard = ({ image, name }: TCategoryCardProps) => {
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+      disable: 'mobile'
+    })
+  })
+
   return (
-    <div className='relative w-full h-[100px] md:h-[280px] overflow-hidden rounded-lg group cursor-pointer'>
-      <img
-        src={image}
-        alt='Category Image'
-        className='w-full h-full object-cover transition-transform duration-500 group-hover:scale-110'
-      />
-      <div className='absolute inset-0 bg-black bg-opacity-50 transition-opacity duration-500 group-hover:bg-opacity-75'></div>
-      <div className='absolute bottom-0 left-0 p-4'>
-        <h3 className='text-white text-3xl font-bold transition-transform duration-500 group-hover:translate-y-1'>
-          {name}
-        </h3>
+    <div data-aos='fade-left' data-aos-delay='10'>
+      <div className='relative w-full h-[100px] md:h-[280px] overflow-hidden rounded-lg group cursor-pointer'>
+        <img
+          src={image}
+          alt='Category Image'
+          className='w-full h-full object-cover transition-transform duration-500 group-hover:scale-110'
+        />
+        <div className='absolute inset-0 bg-black bg-opacity-50 transition-opacity duration-500 group-hover:bg-opacity-75'></div>
+        <div className='absolute bottom-0 left-0 p-4'>
+          <h3 className='text-white text-3xl font-bold transition-transform duration-500 group-hover:translate-y-1'>
+            {name}
+          </h3>
+        </div>
       </div>
     </div>
   )
