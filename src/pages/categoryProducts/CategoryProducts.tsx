a163b1bde2aea33126c0f73209445480f93580e7
miavai649 <mahmudulhaquenoor@gmail.com>
2024-07-12 12:56:11 +0600

    category products page done

--- src/pages/categoryProducts/CategoryProducts.tsx
+++ src/pages/categoryProducts/CategoryProducts.tsx
@@ -1,16 +1,40 @@
+import ProductCard from '@/components/Product/ProductCard'
+import ProductCardSkeleton from '@/components/Product/ProductCardSkeleton'
 import { useGetCategoryProductsQuery } from '@/redux/api/api'
+import { TProduct } from '@/types'
 import { useParams } from 'react-router-dom'
 
 const CategoryProducts = () => {
   const { category } = useParams()
-  const { data } = useGetCategoryProductsQuery({
+  const { data: products, isLoading } = useGetCategoryProductsQuery({
     category
   })
-  console.log('🚀 ~ CategoryProducts ~ data:', data)
+  console.log('🚀 ~ CategoryProducts ~ products:', products)
   return (
-    <div>
-      <h1>This is CategoryProducts component</h1>
-    </div>
+    <section className='container mb-7 md:mb-14'>
+      <h2 className='text-3xl md:text-4xl lg:text-5xl font-bold my-10 text-center'>
+        {category?.toUpperCase()}
+      </h2>
+      {/* Product grid */}
+      <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 mx-auto mt-8'>
+        {products?.data?.map((product: TProduct, i: number) => (
+          <div key={product?._id}>
+            {isLoading ? (
+              // if loading true ==> skeleton
+              <ProductCardSkeleton />
+            ) : (
+              // if loading false ==> product card
+              <ProductCard
+                name={product?.name}
+                images={product?.images}
+                price={product?.price}
+                delay={i * 10}
+              />
+            )}
+          </div>
+        ))}
+      </div>
+    </section>
   )
 }
 
