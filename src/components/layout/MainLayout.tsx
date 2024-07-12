95caa65c6242bafc93b5fdecc255384f14a3c242
miavai649 <mahmudulhaquenoor@gmail.com>
2024-07-13 03:52:27 +0600

    prevent reload when cart have at least one product

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,8 +1,18 @@
 import Footer from '@/pages/shared/Footer'
 import SiteHeader from '@/pages/shared/navbar/SiteHeader'
+import { useAppSelector } from '@/redux/hooks'
 import { Outlet } from 'react-router-dom'
 
 const MainLayout = () => {
+  const { cart } = useAppSelector((state) => state)
+
+  window.onbeforeunload = function (event) {
+    if (cart.length > 0) {
+      event.preventDefault()
+      event.returnValue = ''
+    }
+  }
+
   return (
     <div className='flex flex-col min-h-screen'>
       <SiteHeader />
