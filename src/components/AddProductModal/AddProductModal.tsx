4a5c5fb6ae22d591970db1762b0be7e7ce4c5820
miavai649 <mahmudulhaquenoor@gmail.com>
2024-07-13 11:52:09 +0600

    add product form created

--- src/components/AddProductModal/AddProductModal.tsx
+++ src/components/AddProductModal/AddProductModal.tsx
@@ -20,38 +20,120 @@ const AddProductModal = () => {
           <PlusIcon className='mr-2 h-4 w-4 text-white' /> Add Product
         </Button>
       </DialogTrigger>
-      <DialogContent className='sm:max-w-[425px]'>
+      <DialogContent className='sm:max-w-[600px]'>
         <DialogHeader>
-          <DialogTitle>Edit profile</DialogTitle>
+          <DialogTitle>Add New Product</DialogTitle>
           <DialogDescription>
-            Make changes to your profile here. Click save when you're done.
+            Fill in the details of the new product you want to add. Click save
+            when you're done.
           </DialogDescription>
         </DialogHeader>
-        <div className='grid gap-4 py-4'>
-          <div className='grid grid-cols-4 items-center gap-4'>
-            <Label htmlFor='name' className='text-right'>
-              Name
-            </Label>
-            <Input
-              id='name'
-              defaultValue='Pedro Duarte'
-              className='col-span-3'
-            />
+        <form>
+          {/* product name */}
+          <div className='grid gap-4 py-4'>
+            <div className='grid gap-2'>
+              <Label htmlFor='name'>Name</Label>
+              <input
+                id='name'
+                className='p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-primary-500'
+                required
+              />
+            </div>
+
+            {/* product description */}
+            <div className='grid gap-2'>
+              <Label htmlFor='description'>Description</Label>
+              <input
+                id='description'
+                className='p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-primary-500'
+                required
+              />
+            </div>
+
+            {/* product price and category */}
+            <div className='grid gap-2 md:grid-cols-2 md:gap-4'>
+              <div className='grid gap-2'>
+                <Label htmlFor='price'>Price</Label>
+                <input
+                  id='price'
+                  type='number'
+                  className='p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-primary-500'
+                  required
+                />
+              </div>
+              <div className='grid gap-2'>
+                <Label htmlFor='category'>Category</Label>
+                <select
+                  id='category'
+                  className='p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-primary-500'>
+                  <option value='weights'>Weights</option>
+                  <option value='cardio'>Cardio</option>
+                  <option value='gear'>Gear</option>
+                  <option value='apparel'>Apparel</option>
+                </select>
+              </div>
+            </div>
+
+            {/* product quantity and stock status  */}
+            <div className='grid gap-2 md:grid-cols-2 md:gap-4'>
+              <div className='grid gap-2'>
+                <Label htmlFor='quantity'>Quantity</Label>
+                <input
+                  id='quantity'
+                  type='number'
+                  className='p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-primary-500'
+                  required
+                />
+              </div>
+              <div className='grid gap-2'>
+                <Label htmlFor='stock'>Stock Status</Label>
+                <select
+                  id='stock'
+                  className='p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-primary-500'>
+                  <option value='in-stock'>In Stock</option>
+                  <option value='out-stock'>Out of Stock</option>
+                </select>
+              </div>
+            </div>
+
+            {/* product images */}
+            <div className='grid gap-2'>
+              <Label htmlFor='images'>Images 1</Label>
+              <input
+                id='images'
+                className='p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-primary-500'
+                required
+              />
+            </div>
+            <div className='grid gap-2'>
+              <Label htmlFor='images'>Images 2</Label>
+              <input
+                id='images'
+                className='p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-primary-500'
+                required
+              />
+            </div>
+            <div className='grid gap-2'>
+              <Label htmlFor='images'>Images 3</Label>
+              <input
+                id='images'
+                className='p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-primary-500'
+                required
+              />
+            </div>
+            <div className='grid gap-2'>
+              <Label htmlFor='images'>Images 4</Label>
+              <input
+                id='images'
+                className='p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-primary-500'
+                required
+              />
+            </div>
           </div>
-          <div className='grid grid-cols-4 items-center gap-4'>
-            <Label htmlFor='username' className='text-right'>
-              Username
-            </Label>
-            <Input
-              id='username'
-              defaultValue='@peduarte'
-              className='col-span-3'
-            />
-          </div>
-        </div>
-        <DialogFooter>
-          <Button type='submit'>Save changes</Button>
-        </DialogFooter>
+          <DialogFooter>
+            <Button type='submit'>Save changes</Button>
+          </DialogFooter>
+        </form>
       </DialogContent>
     </Dialog>
   )
